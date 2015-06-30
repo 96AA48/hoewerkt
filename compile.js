63a2dd63895c1ce3091449a22f3c01ae9d6cc437
Bram van der Veen <96aa48@gmail.com>
2015-06-30 11:33:16 +0200

    Heb een "compiler" toegevoegd voor het maken van een papier versie van mijn pws.

---
+++ compile.js
@@ -0,0 +1,12 @@
+//compile.js
+var fs = require('fs');
+var marked = require('marked');
+var buffer = '<a href="http://hoewerkt.werkmanrooster.nl">Een betere leeservaring is op http://hoewerkt.werkmanrooster.nl, het is ook bedoelt om het hier te lezen.</a>';
+
+var articles = fs.readdirSync(__dirname + '/markdown');
+
+for (article of articles) {
+  buffer += marked(fs.readFileSync(__dirname + '/markdown/' + article).toString());
+}
+
+fs.writeFileSync(__dirname + '/pws.html', buffer);
