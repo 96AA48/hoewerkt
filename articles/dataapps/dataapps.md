2429e876a697fc2e0e8b8c4198f06f8ebcf9b496
Bram van der Veen <96aa48@gmail.com>
2016-02-11 09:58:30 +0100

    Kleine aanpassingen gemaakt voor de papieren versie

--- articles/dataapps/dataapps.md
+++
@@ -1,4 +0,0 @@
-## Data gedreven applicaties
----
-Hoe Werkmanrooster werkt, is je waarschijnlijk nu duidelijk. Wat leuk is om te weten, is dat veel van de apps en programma's die jij dagelijks gebruikt, zijn in een zekere zin hetzelfde als Werkmanrooster of rooster.io. Wanneer een app of website veel informatie of data gebruikt, en deze data noodzakelijk is voor het functioneren van die app of website, dan spreken we over een "Data gedreven (web)applicatie". Voorbeelden van dit soort applicaties zijn: Facebook, Whatsapp, Instagram, Twitter, Skype, Marktplaats, Bol.com en Wehkamp. Wat zou Twitter zijn zonder tweets? Wat zou Bol.com zijn zonder producten. Deze applicaties en websites kunnen niet bestaan zonder het bestaan van de nodige data.
-
--- compile.js
+++ compile.js
@@ -1,20 +1,26 @@
 //compile.js
 var fs = require('fs');
 var marked = require('marked');
-var buffer = '<a href="http://hoewerkt.werkmanrooster.nl">Een betere leeservaring is op http://hoewerkt.werkmanrooster.nl, het is ook bedoelt om het hier te lezen. Ook staat dit papier NIET op de bedoelde volgorde.</a>';
+var articles = [
+  'inleiding:Inleiding',
+  'moeilijkheidsgraad:Moeilijkheidsgraad',
+  'inhetkort:In het kort',
+  'intypen:Je gaat naar de website',
+  'opzoek:Op zoek naar je naam',
+  'verkrijgen:Verkrijgen en verwerken',
+  'templates:Templates',
+  'database:Leerlingen, Lokalen en Docenten (database)',
+  'handenuitdemouwen:Handen uit de mouwen',
+  'conclusie:Conclusie',
+  'nawoord:Nawoord'
+];
+// var buffer = '<a href="http://hoewerkt.werkmanrooster.nl">Een betere leeservaring is op http://hoewerkt.werkmanrooster.nl, het is ook bedoelt om het hier te lezen. Ook staat dit papier NIET op de bedoelde volgorde.</a>';
+var buffer = '';
+// var articles = fs.readdirSync(__dirname + '/articles');
 
-var folders = fs.readdirSync(__dirname + '/articles');
-
-for (var folder of folders) {
-  if (folder != 'index.md') {
-    var articles = fs.readdirSync(__dirname + '/articles/' + folder);
-    for (var article of articles) {
-      buffer += marked(fs.readFileSync(__dirname + '/articles/' + folder + '/' + article).toString());
-    }
-  }
-  else {
-    buffer += marked(fs.readFileSync(__dirname + '/articles/' + folder).toString());
-  }
+for (var article of articles) {
+  article = article.split(':')[0];
+  buffer += marked(fs.readFileSync(__dirname + '/articles/' + article + '.md').toString());
 }
 
 fs.writeFileSync(__dirname + '/pws.html', buffer);
